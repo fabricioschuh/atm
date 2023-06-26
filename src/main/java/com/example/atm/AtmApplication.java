92b849695472d16fea122cff2c40b498127d5975
Fabricio Schuh <fabricio.schuh@sap.com>
2023-06-26 11:58:01 -0300

    updating app

--- src/main/java/com/example/atm/AtmApplication.java
+++ src/main/java/com/example/atm/AtmApplication.java
@@ -15,17 +15,18 @@ public class AtmApplication {
 	public static void main(String[] args) throws NotEnoughMoneyException, NotEnoughCombinationException {
 //		SpringApplication.run(AtmApplication.class, args);
 		ATMService atmService = new ATMService();
-//		atmService.depositBill(BillType.ONE,1);
-//		atmService.depositBill(BillType.TWO,1);
-//		atmService.depositBill(BillType.FIVE, 1);
-//		atmService.depositBill(BillType.TEN, 1);
-//		atmService.depositBill(BillType.TWENTY, 1);
+		atmService.depositBill(BillType.ONE,1);
+		atmService.depositBill(BillType.TWO,1);
+		atmService.depositBill(BillType.FIVE, 1);
+		atmService.depositBill(BillType.TEN, 1);
+		atmService.depositBill(BillType.TWENTY, 1);
 		atmService.depositBill(BillType.FIFTY,2);
 		atmService.depositBill(BillType.HUNDRED, 2);
-//
-//		atmService.depositCoin(CoinType.ONE, 1);
-//		atmService.depositCoin(CoinType.FIVE, 1);
+
+		atmService.depositCoin(CoinType.ONE, 1);
+		atmService.depositCoin(CoinType.FIVE, 1);
 		atmService.depositCoin(CoinType.TEN, 1);
+		atmService.depositCoin(CoinType.TWENTYFIVE, 1);
 		atmService.depositCoin(CoinType.FIFTY,1);
 
 		System.out.println("$"+ atmService.getBalance());
